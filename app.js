d48d1a060dc642f9d1c822e29f1b9e46f6657e2d
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-04-07 10:07:43 +0100

    Remove all unnecessary modules

--- app.js
+++ app.js
@@ -9,66 +9,24 @@ let uglifyJs = require('uglify-js');
 let fs = require('fs');
 let passport = require('passport');
 
-require('./app_api/models/db');
 require('./app_api/config/passport');
 
-// var routes = require('./app_server/routes/index');
 let routesApi = require('./app_api/routes/index');
 
 let app = express();
 global.Users = {};
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'app_server', 'views'));
-// app.set('view engine', 'jade');
-
-/* var appClientFiles = [
-  'app_client/app.js',
-  'app_client/home/home.controller.js',
-  'app_client/about/about.controller.js',
-  'app_client/auth/login/login.controller.js',
-  'app_client/auth/register/register.controller.js',
-  'app_client/locationDetail/locationDetail.controller.js',
-  'app_client/reviewModal/reviewModal.controller.js',
-  'app_client/common/services/authentication.service.js',
-  'app_client/common/services/geolocation.service.js',
-  'app_client/common/services/loc8rData.service.js',
-  'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/filters/addHtmlLineBreaks.filter.js',
-  'app_client/common/directives/navigation/navigation.controller.js',
-  'app_client/common/directives/navigation/navigation.directive.js',
-  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
-  'app_client/common/directives/pageHeader/pageHeader.directive.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'
-];
-var uglified = uglifyJs.minify(appClientFiles, { compress : false });
-
-fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("Script generated and saved:", 'loc8r.min.js');
-  }
-});  */
-
 // uncomment after placing your favicon in /public
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/UI/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.urlencoded());
 
-/* app.use(express.static(path.join(__dirname, 'public')));*/
+app.use(express.static(path.join(__dirname, 'UI')));
 
-app.use(express.static(path.join(__dirname, 'app_client')));
-
-// app.use('/', routes);
 app.use('/api', routesApi);
-
-app.get('/api/v1/users/auth/signup', function(req, res){
-  res.sendFile(path.join(__dirname, 'app_client', 'user', 'auth', 'signup.html'))
-})
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'UI', 'index.html'));
