c5ec748ce8f400b777761fbb5b7e99096dbad3a2
NawasNaziru <nawasnaziru@gmail.com>
2019-04-20 04:41:12 +0100

    Update and rename signup_form.js to admin_all_bank_accounts.js

---
+++ scripts/admin_all_bank_accounts.js
@@ -0,0 +1,22 @@
+
+function getElemVal(elem) {
+  return document.getElementById(elem).value;
+}
+
+const submitForm = function (formId) {
+  document.getElementById(formId).addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!getElemVal('input_button')) {
+      document.getElementById('message').innerHTML = '';
+      document.getElementById('message').classList.add('invalid');
+      document.getElementById('validation_msg').innerHTML = 'All fields are required. Try again.';
+      return;
+    }
+
+    document.getElementById('message').innerHTML = 'Please wait...';
+    document.getElementById('message').innerHTML = 'Successful!';
+    document.getElementById('validation_msg').innerHTML = '';
+  })
+};
+// Handler for submitting create_acct form
+submitForm('forms');
